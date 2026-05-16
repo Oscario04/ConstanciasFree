c4b19c5729c348d425a4eeb4d00cf07ffcfb7e1c
NYW2811 <ian.1r0ngc@gmail.com>
2026-05-16 10:44:23 -0600

    fix: make local fullstack dev runnable

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,4 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-    globals: true,
-  },
 });
-
